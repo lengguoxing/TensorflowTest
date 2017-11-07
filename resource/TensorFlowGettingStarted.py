53a7ef051bd3bf3e9dbffff619a2172115622da8
lengguoxing <626826586@qq.com>
2017-11-07 18:03:58 +0800

    add some code

--- resource/TensorFlowGettingStarted.py
+++ resource/TensorFlowGettingStarted.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 node1 = tf.constant(3.0, dtype=tf.float32)
-node2 = tf.constant(4.0) # also tf.float32 implicitly
+node2 = tf.constant(4.0)  # also tf.float32 implicitly
 print(node1, node2)
 
 sess = tf.Session()
@@ -30,6 +30,7 @@ b = tf.Variable([-.3], dtype=tf.float32)
 x = tf.placeholder(tf.float32)
 linear_model = W*x + b
 
+#  变量初始化
 init = tf.global_variables_initializer()
 sess.run(init)
 
@@ -39,14 +40,14 @@ print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
 y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
-print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+print("loss function", sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 
 ss = (0-0)*(0-0) + (0.3+1)*(0.3+1) + (0.6+2)*(0.6+2) + (0.9+3)*(0.9+3)  # 真实算法
-print(ss)
+print("真实算法ss", ss)
 
 print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, 0.3, 0.6, 0.9]}))  # 测试参数
 
-# ft.assign
+# ft.assign 变量重新赋值
 fixW = tf.assign(W, [-1.])
 fixb = tf.assign(b, [1.])
 sess.run([fixW, fixb])
@@ -55,8 +56,8 @@ print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 
 
 # tf.train API
-optimizer = tf.train.GradientDescentOptimizer(0.01)
-train = optimizer.minimize(loss)
+optimizer = tf.train.GradientDescentOptimizer(0.01)  # 梯度下降优化器
+train = optimizer.minimize(loss)    # 最小化损失函数
 sess.run(init)  # reset values to incorrect defaults.
 for i in range(1000):
   sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
@@ -64,9 +65,9 @@ for i in range(1000):
 print(sess.run([W, b]))
 
 
-print("------------------------------------")
+print("------------------------------------1")
 
-# Complete program:The completed trainable linear regression model is shown here:
+# Complete program:The completed trainable linear regression model is shown here:完整的训练线性回归模型代码
 # Model parameters
 W = tf.Variable([.3], dtype=tf.float32)
 b = tf.Variable([-.3], dtype=tf.float32)
@@ -76,7 +77,7 @@ linear_model = W*x + b
 y = tf.placeholder(tf.float32)
 
 # loss
-loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+loss = tf.reduce_sum(tf.square(linear_model - y))  # sum of the squares
 # optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
@@ -96,9 +97,9 @@ curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
 print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
 
 
-print("------------------------------------")
+print("------------------------------------2")
 
-# tf.estimator
+# tf.estimator  使用tf.estimator实现上述训练
 # Notice how much simpler the linear regression program becomes with tf.estimator:
 # NumPy is often used to load, manipulate and preprocess data.
 import numpy as np
@@ -133,6 +134,52 @@ eval_input_fn = tf.estimator.inputs.numpy_input_fn(
 # training data set.
 estimator.train(input_fn=input_fn, steps=1000)
 
+# Here we evaluate how well our model did.
+train_metrics = estimator.evaluate(input_fn=train_input_fn)
+eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+print("train metrics: %r"% train_metrics)
+print("eval metrics: %r"% eval_metrics)
+
+
+print("------------------------------------3")
+
+# A custom model：客户自定义实现训练
+# Declare list of features, we only have one real-valued feature
+def model_fn(features, labels, mode):
+  # Build a linear model and predict values
+  W = tf.get_variable("W", [1], dtype=tf.float64)
+  b = tf.get_variable("b", [1], dtype=tf.float64)
+  y = W*features['x'] + b
+  # Loss sub-graph
+  loss = tf.reduce_sum(tf.square(y - labels))
+  # Training sub-graph
+  global_step = tf.train.get_global_step()
+  optimizer = tf.train.GradientDescentOptimizer(0.01)
+  train = tf.group(optimizer.minimize(loss),
+                   tf.assign_add(global_step, 1))
+  # EstimatorSpec connects subgraphs we built to the
+  # appropriate functionality.
+  return tf.estimator.EstimatorSpec(
+      mode=mode,
+      predictions=y,
+      loss=loss,
+      train_op=train)
+
+estimator = tf.estimator.Estimator(model_fn=model_fn)
+# define our data sets
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7., 0.])
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+
+# train
+estimator.train(input_fn=input_fn, steps=1000)
 # Here we evaluate how well our model did.
 train_metrics = estimator.evaluate(input_fn=train_input_fn)
 eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
